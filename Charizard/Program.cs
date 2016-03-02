61021a50653f3902e26fa298ea17773723f985b6
your Name <you@example.com>
2016-03-02 10:12:14 -0500

    setting up charizard

---
+++ Charizard/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charizard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string nurse = "Mary had a little lamb,\n its fleece\t was \"white\" as snow.";
+            //Console.WriteLine(nurse);
+
+            int x = 1;
+            int y = x++;
+            Console.WriteLine(x);
+    }
+}
